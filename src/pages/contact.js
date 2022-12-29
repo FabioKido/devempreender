7a260166ef66db742e98b01bbf9775852060c71a
FabioKido <fabiohenryquemesquita@gmail.com>
2022-12-29 13:40:49 -0300

    add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Contact = () => {
+    return(
+        <div>contact</div>
+    )
+}
+
+export default Contact
+
+export const Head = () => <title>contact</title>
