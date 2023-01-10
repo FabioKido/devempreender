3153847736ee401e3335da82e27f6fe53b66d39a
FabioKido <fabiohenryquemesquita@gmail.com>
2023-01-10 19:37:56 -0300

    updated home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,9 @@ import * as React from "react"
 
 const IndexPage = () => {
   return (
-    <h1>Início</h1>
+    <main className="bg-white h-screen">
+      <img src="https://images.unsplash.com/photo-1580927752452-89d86da3fa0a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" alt="mac-notebook"></img>
+    </main>
   )
 }
 
